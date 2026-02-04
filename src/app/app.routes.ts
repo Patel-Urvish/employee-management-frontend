2c9ab7b7d3e02a2c7faf5ad6f90aa80f68da05fe
urvish <urvishpatel0914@gmail.com>
2026-02-04 20:39:40 +0530

    component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Header } from './header/header';
+import { Dashboard } from './dashboard/dashboard';
+import { Employee } from './employee/employee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+
+    },
+    {
+        path:'',
+        component:Header,
+        children:[
+            {
+                path:'dashboard',
+                component:Dashboard
+            },
+            {
+                path:'new-employee',
+                component:Employee
+            }
+        ]
+    }
+];
---
+++ src/app/employeelist/employeelist.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeelist',
+  imports: [],
+  templateUrl: './employeelist.html',
+  styleUrl: './employeelist.css',
+})
+export class Employeelist {
+
+}
